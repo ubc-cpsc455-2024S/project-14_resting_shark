c9c174c24248d4ee4c355c5ed1223cc870111faa
maggie <maggie.weng112@gmail.com>
2024-05-31 11:48:11 -0700

    slightly refactored Lesson.tsx

--- frontend/src/pages/Lesson.tsx
+++ frontend/src/pages/Lesson.tsx
@@ -25,8 +25,9 @@ import { motion, AnimatePresence } from "framer-motion";
 export default function Lesson({ contentList }: { contentList: Content[] }) {
   const [pageNumber, setPageNumber] = useState(0);
   const [buttonText, setButtonText] = useState("Let's Go!");
-  const [direction, setDirection] = useState("");
+  const [direction, setDirection] = useState("forward");
 
+  // Changes button text
   useEffect(() => {
     if (contentList[pageNumber].type === "intro") {
       setButtonText("Let's Go!");
@@ -76,6 +77,7 @@ export default function Lesson({ contentList }: { contentList: Content[] }) {
 
   const renderedPage = renderPage(contentList[pageNumber]);
 
+  // Main Page
   return (
     <div className="container">
       <Header />
@@ -84,6 +86,57 @@ export default function Lesson({ contentList }: { contentList: Content[] }) {
   );
 
   function Header() {
+    return (
+      <div className="head">
+        <div className=" left-head">
+          <ExitButton />
+        </div>
+        <div className="middle-head">
+          <ProgressHeader />
+        </div>
+        <div className="right-head">
+          <EditButton />
+        </div>
+      </div>
+    );
+  }
+
+  function ExitButton() {
+    return (
+      <div className="left-container">
+        <Link className="exit-link" to="/">
+          <button className="exit-button">
+            <LuX size={22} color={"#4369ee"} />
+            <span>Exit</span>
+          </button>
+        </Link>
+      </div>
+    );
+  }
+
+  function ProgressHeader() {
+    return (
+      <div className="progress-container">
+        <div className="progress-stats">
+          <div className="stat">
+            <img src="/Lesson/heart.png" alt="heart" />
+            <span>3</span>
+          </div>
+          <div className="page-count">
+            {pageNumber + 1}/{contentList.length}
+          </div>
+          <div className="stat">
+            <img src="/Lesson/streak.png" alt="streak" />
+            <span>0</span>
+          </div>
+          <div></div>
+        </div>
+        <ProgressBar />
+      </div>
+    );
+  }
+
+  function ProgressBar() {
     const onNextButtonPress = () => {
       if (pageNumber + 1 < contentList.length) {
         setDirection("forward");
@@ -99,83 +152,90 @@ export default function Lesson({ contentList }: { contentList: Content[] }) {
     };
 
     return (
-      <div className="head">
-        <div className=" left-head">
-          <div className="left-container">
-            <Link className="exit-link" to="/">
-              <button className="exit-button">
-                <LuX size={22} color={"#4369ee"} />
-                <span>Exit</span>
-              </button>
-            </Link>
-          </div>
+      <div className="progress-bar-container">
+        <button onClick={onBackButtonPress}>
+          <LuChevronLeft
+            className={pageNumber == 0 ? "inactive-icon" : "active-icon"}
+            size={28}
+          />
+        </button>
+        <div className="progress-bar">
+          <AnimatePresence>
+            <motion.div
+              className="progress-tracker"
+              initial={{
+                width:
+                  direction === "forward"
+                    ? `${(pageNumber / contentList.length) * 100}%`
+                    : `${((pageNumber + 2) / contentList.length) * 100}%`,
+              }}
+              animate={{
+                width: `${((pageNumber + 1) / contentList.length) * 100}%`,
+              }}
+              transition={{ duration: 0.3 }}
+            />
+          </AnimatePresence>
         </div>
-        <div className="middle-head">
-          <div className="progress-container">
-            <div className="progress-stats">
-              <div className="stat">
-                <img src="/Lesson/heart.png" alt="heart" />
-                <span>3</span>
-              </div>
-              <div className="page-count">
-                {pageNumber + 1}/{contentList.length}
-              </div>
-              <div className="stat">
-                <img src="/Lesson/streak.png" alt="streak" />
-                <span>0</span>
-              </div>
-              <div></div>
-            </div>
-            <div className="progress-bar-container">
-              <button onClick={onBackButtonPress}>
-                <LuChevronLeft
-                  className={pageNumber == 0 ? "inactive-icon" : "active-icon"}
-                  size={28}
-                />
-              </button>
-              <div className="progress-bar">
-                <AnimatePresence>
-                  <motion.div
-                    className="progress-tracker"
-                    initial={{
-                      width:
-                        direction === "forward"
-                          ? `${(pageNumber / contentList.length) * 100}%`
-                          : `${((pageNumber + 2) / contentList.length) * 100}%`,
-                    }}
-                    animate={{
-                      width: `${
-                        ((pageNumber + 1) / contentList.length) * 100
-                      }%`,
-                    }}
-                    transition={{ duration: 0.5 }}
-                  />
-                </AnimatePresence>
-              </div>
-              <button onClick={onNextButtonPress}>
-                <LuChevronRight
-                  className={
-                    pageNumber + 1 == contentList.length
-                      ? "inactive-icon"
-                      : "active-icon"
-                  }
-                  size={28}
-                />
-              </button>
-            </div>
-          </div>
+        <button onClick={onNextButtonPress}>
+          <LuChevronRight
+            className={
+              pageNumber + 1 == contentList.length
+                ? "inactive-icon"
+                : "active-icon"
+            }
+            size={28}
+          />
+        </button>
+      </div>
+    );
+  }
+
+  function EditButton() {
+    return (
+      <button className="edit-button">
+        <LuPenSquare size={23} />
+        <span>Edit Lesson</span>
+      </button>
+    );
+  }
+
+  function Body() {
+    return (
+      <div className="body">
+        <div className="left-body">
+          <ChaptersNav />
         </div>
-        <div className="right-head">
-          <button className="edit-button">
-            <LuPenSquare size={23} />
-            <span>Edit Lesson</span>
-          </button>
+        <div className="middle-body">
+          <MainDisplay />
+        </div>
+        <div className="right-body">
+          <AIHelper />
         </div>
       </div>
     );
   }
 
-  function Body() {
+  function ChaptersNav() {
+    return (
+      <div className="left-container">
+        <div className="chapters">
+          <h1>Astronomy</h1>
+          <div className="list-container">
+            <ul>
+              <li className="active">Intro</li>
+              <li>The Sun</li>
+              <li>Galaxies</li>
+              <li>The Solar System</li>
+              <li>Planets</li>
+              <li>Stars</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  function MainDisplay() {
     const onNextButtonPress = () => {
       setTimeout(() => {
         setDirection("forward");
@@ -186,49 +246,33 @@ export default function Lesson({ contentList }: { contentList: Content[] }) {
     };
 
     return (
-      <div className="body">
-        <div className="left-body">
-          <div className="left-container">
-            <div className="chapters">
-              <h1>Astronomy</h1>
-              <div className="list-container">
-                <ul>
-                  <li className="active">Intro</li>
-                  <li>The Sun</li>
-                  <li>Galaxies</li>
-                  <li>The Solar System</li>
-                  <li>Planets</li>
-                  <li>Stars</li>
-                </ul>
-              </div>
+      <>
+        <div className="main-display">{renderedPage}</div>
+        <motion.button
+          layout
+          transition={{ duration: 0.2 }}
+          className="next-button"
+          onClick={onNextButtonPress}
+        >
+          <div className="inner-button">{buttonText}</div>
+        </motion.button>
+      </>
+    );
+  }
+
+  function AIHelper() {
+    return (
+      <div className="ai-container">
+        <div className="header">Need Help?</div>
+        <div className="chat-bottom-border">
+          <div className="chat-container">
+            <div className="chat">
+              <LuBot size={30} />
             </div>
-          </div>
-        </div>
-        <div className="middle-body">
-          <div className="main-display">{renderedPage}</div>
-          <motion.button
-            layout
-            transition={{ duration: 0.2 }}
-            className="next-button"
-            onClick={onNextButtonPress}
-          >
-            <div className="inner-button">{buttonText}</div>
-          </motion.button>
-        </div>
-        <div className="right-body">
-          <div className="ai-container">
-            <div className="header">Need Help?</div>
-            <div className="chat-bottom-border">
-              <div className="chat-container">
-                <div className="chat">
-                  <LuBot size={30} />
-                </div>
-                <div className="chat-button-container">
-                  <button>
-                    <span>I want a Hint!</span>
-                  </button>
-                </div>
-              </div>
+            <div className="chat-button-container">
+              <button>
+                <span>I want a Hint!</span>
+              </button>
             </div>
           </div>
         </div>
