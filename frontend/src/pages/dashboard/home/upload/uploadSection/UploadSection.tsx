5302472d8f726848d3b5817b497fdf65255e7255
maggie <maggie.weng112@gmail.com>
2024-06-30 15:41:58 -0700

    upload section

--- frontend/src/pages/dashboard/home/upload/uploadSection/UploadSection.tsx
+++ frontend/src/pages/dashboard/home/upload/uploadSection/UploadSection.tsx
@@ -1,22 +1,16 @@
 import { LuFolderUp } from "react-icons/lu";
-import s from "./UploadSection.module.css"
+import s from "./UploadSection.module.css";
 
 export default function UploadSection() {
   return (
-     <div className={s.uploadSectionContainer}>
-        <LuFolderUp />
-        <div className={s.uploadFilePrompt}>
-          <div>
-            Click to upload&nbsp;
-          </div>
-          <div>
-            or drag and drop
-          </div>
-        </div>
-        
-        <div>
-          Maximum file size: 50mb
-        </div>
-     </div>
-  )
+    <div className={s.uploadSectionContainer}>
+      <LuFolderUp className={s.icon} />
+      <div className={s.uploadFilePrompt}>
+        <button className={s.uploadButton}>Click to upload</button>
+        <div>or drag and drop</div>
+      </div>
+
+      <div className={s.maxFileSize}>Maximum file size: 50mb</div>
+    </div>
+  );
 }
