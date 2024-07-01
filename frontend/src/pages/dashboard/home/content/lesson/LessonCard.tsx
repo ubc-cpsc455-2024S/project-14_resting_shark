e3fb46db6c369fe76f75557e41bbcd51283f36b6
maggie <maggie.weng112@gmail.com>
2024-06-30 17:13:30 -0700

    yay :D

--- frontend/src/pages/dashboard/home/content/lesson/LessonCard.tsx
+++ frontend/src/pages/dashboard/home/content/lesson/LessonCard.tsx
@@ -1,40 +1,57 @@
 import { Link } from "react-router-dom";
 import s from "./LessonCard.module.css";
-import { LuHeart, LuDot } from "react-icons/lu";
+import { LuDot } from "react-icons/lu";
+import { GoHeartFill } from "react-icons/go";
 
 export default function Lesson({ lesson }: any) {
   const TOTAL_LIVES = 3;
 
   const lives = Array.from({ length: TOTAL_LIVES }, (_, index) => {
     if (index < lesson.lives) {
-      return <LuHeart key={index} />;
+      return <GoHeartFill className={s.heart} key={index} />;
     } else {
-      return <LuDot key={index} />;
+      return <LuDot className={s.dot} key={index} />;
     }
   });
 
-
   return (
-    <Link to={`/lesson/${lesson.id}`}>
+    <Link className={s.link} to={`/lesson/${lesson.id}`}>
       <div className={s.container}>
-        {lives}
-        <div>
-          {lesson.name}
+        <div className={s.livesContainer}>{lives}</div>
+        <div className={s.bottomContainer}>
+          <span className={s.name}>{lesson.name}</span>
+          <span className={s.totalQuestions}>
+            {lesson.totalQuestions} questions
+          </span>
+          <ProgressBar
+            percentage={
+              (lesson.completedQuestions / lesson.totalQuestions) * 100
+            }
+          />
+          <button className={s.continueToLearnButton}>Continue to learn</button>
         </div>
-        <div>
-          {lesson.totalQuestions} questions
-        </div>
-        <ProgressBar percentage={lesson.completedQuestions / lesson.totalQuestions * 100} />
-        <button className={s.continueToLearnButton}>
-          Continue to learn
-        </button>
       </div>
     </Link>
   );
 }
 
-function ProgressBar({ percentage }:{percentage: number}) {
+function ProgressBar({ percentage }: { percentage: number }) {
   return (
-    <div>Percentage: {Math.floor(percentage)}</div>
-  )
+    <div className={s.progressBarContainer}>
+      <div className={s.progressContainer}>
+        <div
+          className={s.progressIndicator}
+          style={
+            percentage === 100
+              ? {
+                  backgroundColor: "#29CC60",
+                  width: `${Math.floor(percentage)}%`,
+                }
+              : { width: `${Math.floor(percentage)}%` }
+          }
+        ></div>
+      </div>
+      <span className={s.percentage}>{Math.floor(percentage)}%</span>
+    </div>
+  );
 }
