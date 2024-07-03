1b26859e576eb9db826818b54a874c7ba105867f
atkv2 <128923743+atkv2@users.noreply.github.com>
2024-07-03 08:03:46 -0700

    basic register page style

--- frontend/src/pages/Auth/Register.tsx
+++ frontend/src/pages/Auth/Register.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAppDispatch } from "../../redux/hooks";
 import { authApi } from "../../api/authApi";
+import "./Register.css";
 
 export default function Register() {
   const dispatch = useAppDispatch();
@@ -22,29 +23,40 @@ export default function Register() {
   };
 
   return (
-    <div>
-      <div>
-        <label>
-          Username:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
+    <div className="create-account-container">
+      <div className="left-container">
+        <div className="left-content">
+          <h1>Learn with Study Swan</h1>
+          <p>Join our learning platform to access interactive lessons and customized for your needs and interests!</p>
+        </div>
       </div>
-      <div>
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <button onClick={handleRegister}>Register</button>
+      <div className="right-container">
+        <div className="right-content">
+          <h2>Create an Account</h2>
+          <div>
+            <label>
+              Username:
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Password:
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <button onClick={handleRegister}>Register</button>
+          </div>
+        </div>
       </div>
     </div>
   );
