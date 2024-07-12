ab6cdf9f0b2a1ae8d0c26de1133cc113f1a145ad
maggie <maggie.weng112@gmail.com>
2024-07-12 05:23:36 -0700

    more stuff idk

--- frontend/src/pages/dashboard/profile/Profile.tsx
+++ frontend/src/pages/dashboard/profile/Profile.tsx
@@ -3,6 +3,7 @@ import StatDisplay from "./graphs/stat/StatDisplay";
 import LogoutButton from "./LogoutButton/LogoutButton";
 import ProfileDisplay from "./profile-display/ProfileDisplay";
 import s from "./Profile.module.css";
+import UserInfo from "./user-info/UserInfo";
 
 export default function Profile() {
   return (
@@ -17,6 +18,7 @@ export default function Profile() {
       </div>
       <div className={s.profileContainer}>
         <ProfileDisplay />
+        <UserInfo />
       </div>
     </div>
   );
