0efdc0709a18531468c3c51d29686129a98e5efd
ehuang07 <huangsimiao2014@gmail.com>
2024-07-13 09:15:49 -0700

    backend

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,6 +1,7 @@
 import express, { Router, Request, Response } from 'express';
 import authMiddleware from '../middleware/authMiddleware';
 import userService from '../services/userService';
+import ErrorWithCode from '../errors/ErrorWithCode';
 
 const router: Router = express.Router();
 
@@ -9,12 +10,27 @@ deletes a user by id from jwt token
 */
 router.delete('/', authMiddleware, async (req: Request, res: Response) => {
   try {
-    console.log(req.user.id)
     const userId = req.user.id;
     const lessons = await userService.deleteUser(userId);
     res.status(200).json(lessons);
   } catch (error: any) {
     res.status(500).json({ message: error.message });
+  }
+});
+
+
+/*
+gets stats for a user. TODO: this is used for profile page, edit model to include all info needed to return full stats
+*/
+router.get('/stats', authMiddleware, async (req: Request, res: Response) => {
+  try {
+    const userId = req.user.id;
+    const stats = await userService.getUserStats(userId);
+    console.log(userId)
+    console.log(stats)
+    res.status(200).json(stats);
+  } catch (error: any) {
+    res.status(error.code || 500).json({ message: error.message });
   }
 });
 
