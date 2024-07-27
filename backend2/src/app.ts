043c1bda8eb89e5ee4b24770ef9472b98ba18cd7
ehuang07 <huangsimiao2014@gmail.com>
2024-07-27 00:45:47 -0700

    move

--- backend2/src/app.ts
+++ backend2/src/app.ts
@@ -1,11 +1,42 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+
+// import { authRouter } from "./routes/auth";
+// import { protectedRouter } from "./routes/protectedRoute";
+// import { lessonsRouter } from "./routes/lesson";
+// import { usersRouter } from "./routes/user";
+import { connectDB } from "./db/mongoConnection";
+
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// middleware
+app.use(cors());
+dotenv.config();
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+// routes
+// app.use("/auth", authRouter);
+// app.use("/protected", protectedRouter);
+// app.use("/lesson", lessonsRouter);
+// app.use("/user", usersRouter);
+
+// server port
+const PORT: number = 3000;
+app.listen(PORT, async () => {
+  console.log(`App listening on port ${PORT}`);
+
+  try {
+    connectDB();
+    console.log("Connected To Database");
+  } catch (error) {
+    console.error(error);
+  }
 });
