8450c9a600e0c8a67cbdc34749386a69b08a0dce
ehuang07 <huangsimiao2014@gmail.com>
2024-08-05 22:36:36 -0700

    search and sort

--- frontend/src/pages/dashboard/home/Home.tsx
+++ frontend/src/pages/dashboard/home/Home.tsx
@@ -3,13 +3,25 @@ import GreetingAndSearch from "./header/greetingAndSearch/GreetingAndSearch";
 import Header from "./header/Header";
 import s from "./Home.module.css";
 import * as React from "react";
+import { useState } from "react";
 
 export default function Home() {
+  const [searchValue, setSearchValue] = useState("");
+  const [sortValue, setSortValue] = useState("recent")
+
   return (
     <div className={`${s.homeContainer}`}>
-      <GreetingAndSearch />
+      <GreetingAndSearch 
+        searchValue={searchValue} 
+        setSearchValue={setSearchValue}
+        sortValue={sortValue}
+        setSortValue={setSortValue}
+      />
       <Header />
-      <Content />
+      <Content 
+        searchValue={searchValue} 
+        sortValue={sortValue} 
+      />
     </div>
   );
 }
--- frontend/src/pages/dashboard/home/content/Content.tsx
+++ frontend/src/pages/dashboard/home/content/Content.tsx
@@ -5,7 +5,12 @@ import LessonCard from "./lesson/LessonCard";
 import s from "./Content.module.css";
 import * as React from "react";
 
-export default function Content() {
+interface SearchAndSortProps {
+  searchValue: string,
+  sortValue: string,
+}
+
+export default function Content({ searchValue, sortValue }: SearchAndSortProps) {
   const lessons = useAppSelector((state) => state.dashboardPage.lessons);
   const token = useAppSelector((state) => state.auth.jwtToken);
   const dispatch = useAppDispatch();
@@ -15,7 +20,23 @@ export default function Content() {
     dispatch(lessonApi.fetchLessons(token));
   }, [dispatch]);
 
-  const lessonCards = lessons.map((item, index) => (
+  debugger;
+
+  let lessonsCopy = JSON.parse(JSON.stringify(lessons));
+
+  const sortedLessons = sortValue === "recent"
+  ? lessonsCopy 
+  : lessonsCopy.sort((a: any, b: any) => {
+      if (a.name < b.name) return -1; 
+      if (a.name > b.name) return 1;  
+      return 0;
+    });
+
+  const filteredLessons = sortedLessons.filter((lesson: any) => {
+    return lesson.name.includes(searchValue);
+  });
+
+  const lessonCards = filteredLessons.map((item: any, index: number) => (
     <LessonCard isFirst={index === 0} lesson={item} key={index} />
   ));
 
